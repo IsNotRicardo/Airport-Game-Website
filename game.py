22f80258c8746762f637295073a99c446b029100
upeksha <slusanjeewani@gmail.com>
2023-12-12 11:56:04 +0200

    Tutorial HTML

--- game.py
+++ game.py
@@ -7,8 +7,8 @@ connection = mysql.connector.connect(
     host='127.0.0.1',
     port='3306',
     database='flight_game',
-    user='dbuser',
-    password='1234',
+    user='root',
+    password='Soft2023@fin',
     autocommit=True
 )
 
